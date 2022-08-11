19c66a55327a25e7c8c82e1e232dcd3b11f56a94
magical <sun920122@gmail.com>
2022-08-12 05:35:50 +0800

    Create imageandsound.py
    Encode and decode image and sound
    file with base64.

---
+++ src/imageandsound.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+import base64
+from io import BytesIO
+from os import path, mkdir
+
+from PIL import Image
+
+
+def img_encode(image_file, base64_file):
+    with open(image_file, 'rb') as image:
+        temp = f'image = {base64.b64encode(image.read())}'
+
+    with open(base64_file, 'w') as f:
+        f.write(temp)
+
+
+def img_decode():
+    dir = path.join('.', 'img')
+    if not path.isdir(dir):
+        mkdir(dir)
+    from image_base64 import image
+    image = base64.b64decode(image)
+    image = BytesIO(image)
+    Image.open(image).save(path.join(dir, 'image.jpg'))
+
+
+def sound_encode(image_file, base64_file):
+    with open(image_file, 'rb') as image:
+        temp = f'sound = {base64.b64encode(image.read())}'
+
+    with open(base64_file, 'w') as f:
+        f.write(temp)
+
+
+def sound_decode():
+    dir = path.join('.', 'sound')
+    if not path.isdir():
+        mkdir(dir)
+    from image_base64 import image
+    image = base64.b64decode(image)
+    image = BytesIO(image)
+    Image.open(image).save(path.join(dir, 'sound.mp3'))
+
+if __name__ == '__main__':
+    base64_file = path.join('.', 'src', 'image_base64.py')
+    image_file = path.join('.', 'img', 'image.jpg')
+    test_file = path.join('.', 'img', 'test.jpg')
+    try:
+        img_encode(image_file, base64_file)
+    except:
+        img_encode(test_file, base64_file)
+    base64_file = path.join('.', 'src', 'sound_base64.py')
+    image_file = path.join('.', 'sound', 'sound.mp3')
+    test_file = path.join('.', 'sound', 'test.mp3')
+    try:
+        sound_encode(image_file, base64_file)
+    except:
+        sound_encode(test_file, base64_file)
