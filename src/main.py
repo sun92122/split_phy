eca219c6ac53d213e5f5ff85048250e3440643ff
magical <sun920122@gmail.com>
2022-08-12 07:23:19 +0800

    Update main.py
    Clean window

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 from PyQt5 import QtWidgets
 
 from core import MainWindow
 
 if __name__ == '__main__':
-    import sys
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
+    os.system('cls' if os.name == 'nt' else 'clear')
     window.show()
     sys.exit(app.exec_())
