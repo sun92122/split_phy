3888d0f65a7e17f76153560e87aac432c14fd407
magical <sun920122@gmail.com>
2022-08-12 05:27:50 +0800

    Add some foolproof
    Pressing the second button within five seconds will now not trigger.

--- src/core.py
+++ src/core.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
 from os import path
+from time import perf_counter
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from debugger import new_log, timing
 from UI import UiMain
+from imageandsound import img_decode, sound_decode
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -17,6 +19,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         self.connect_button()
         self.set_button_icon()
+        self.timer = perf_counter()-5
 
     @timing    
     def connect_button(self):
@@ -27,24 +30,28 @@ class MainWindow(QtWidgets.QMainWindow):
                 button.clicked.connect(self.push_button)
 
     @timing
-    def set_button_icon(self): # FIX 2022/8/8 magical
+    def set_button_icon(self):
+        debug = False
+        img_decode()
+        image = './img/'+('test.jpg' if debug else 'image.jpg')
         for R in range(1, 6):
             for C in range(1, 6):
                 button: QtWidgets.QPushButton = getattr(self.ui, f'R{R}C{C}')
-                try: # TODO 2022/8/9 magical set button icon
-                    button.setStyleSheet("""
-                        QPushButton {
-                            border-image: url('./img/test.jpg');
-                        }
-                    """)
+                try:
+                    button.setStyleSheet('QPushButton {'+' border-image: url("{}"); '.format(image)+'}')
                 except:
                     self.logger.warning(f"Button {button.objectName()} icon import failed")
         
     @timing
     def push_button(self, checked):
         button: QtWidgets.QPushButton = self.sender()
+        if perf_counter()-self.timer > 5:
+            self.timer = perf_counter()
+            self.logger.info(f"Button {button.objectName()} has been pressed")
+        else:
+            self.logger.info(f"Button {button.objectName()} has been pressed but still on cooldown")
+            return
         # button = self.ui.R1C1 # test
-        self.logger.info(f"Button {button.objectName()} has been pressed")
         orig = button.geometry()
         dwidth = orig.width()//100 if orig.width() > 200 else 2
 
@@ -79,12 +86,12 @@ class MainWindow(QtWidgets.QMainWindow):
             if not sub_width(i):
                 break
         
-        button.setStyleSheet("""
-                        QPushButton {
-                            border-image: none;
-                        }
-                    """)
+        button.setStyleSheet('QPushButton {'+' border-image: url("{}"); '.format('none')+'}')
 
         for i in range(100):
             if not add_width(i):
                 break
+
+
+    def play_sound(self):
+        sound_decode()
