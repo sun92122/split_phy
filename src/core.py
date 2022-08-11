96fc9f7dc9d1fdd3d1186483a2ed033b2b07d7b5
magical <sun920122@gmail.com>
2022-08-12 07:24:39 +0800

    Update core.py
    Sound play method done

--- src/core.py
+++ src/core.py
@@ -3,11 +3,12 @@
 from os import path
 from time import perf_counter
 
+from pygame import mixer
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from debugger import new_log, timing
-from UI import UiMain
 from imageandsound import img_decode, sound_decode
+from UI import UiMain
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -20,8 +21,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.connect_button()
         self.set_button_icon()
         self.timer = perf_counter()-5
+        self.set_push_sound()
+        self.play_sound = lambda: mixer.music.play()
 
-    @timing    
+    @timing
     def connect_button(self):
         # QPushButton documentation: https://doc.qt.io/qtforpython-5/PySide2/QtWidgets/QPushButton.html
         for R in range(1, 6):
@@ -32,26 +35,45 @@ class MainWindow(QtWidgets.QMainWindow):
     @timing
     def set_button_icon(self):
         debug = False
-        img_decode()
+        if not path.exists(path.join('.', 'img', 'test.jpg' if debug else 'image.jpg')):
+            img_decode()
         image = './img/'+('test.jpg' if debug else 'image.jpg')
         for R in range(1, 6):
             for C in range(1, 6):
                 button: QtWidgets.QPushButton = getattr(self.ui, f'R{R}C{C}')
                 try:
-                    button.setStyleSheet('QPushButton {'+' border-image: url("{}"); '.format(image)+'}')
+                    button.setStyleSheet(
+                        'QPushButton {'+' border-image: url("{}"); '.format(image)+'}')
                 except:
-                    self.logger.warning(f"Button {button.objectName()} icon import failed")
-        
+                    self.logger.warning(
+                        f"Button {button.objectName()} icon import failed")
+
+    @timing
+    def set_push_sound(self):
+        debug = False
+        sound_path = path.join(
+            '.', 'sound', 'test.mp3' if debug else 'sound.mp3')
+        if not path.exists(sound_path):
+            sound_decode()
+        mixer.init()
+        mixer.music.load(sound_path)
+
     @timing
     def push_button(self, checked):
         button: QtWidgets.QPushButton = self.sender()
-        if perf_counter()-self.timer > 5:
+        debug = False
+        if perf_counter()-self.timer > 5 or debug:
             self.timer = perf_counter()
             self.logger.info(f"Button {button.objectName()} has been pressed")
         else:
-            self.logger.info(f"Button {button.objectName()} has been pressed but still on cooldown")
+            self.logger.info(
+                f"Button {button.objectName()} has been pressed but still on cooldown")
             return
-        # button = self.ui.R1C1 # test
+
+        self.play_sound()
+        self.flip_button(button)
+
+    def flip_button(self, button: QtWidgets.QPushButton):
         orig = button.geometry()
         dwidth = orig.width()//100 if orig.width() > 200 else 2
 
@@ -64,8 +86,9 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.flash()
                 return False
             else:
-                dx = (dwidth+(i%2))//2
-                button.setGeometry(button.geometry()-QtCore.QMargins(dx, 0, dwidth, 0))
+                dx = (dwidth+(i % 2))//2
+                button.setGeometry(button.geometry() -
+                                   QtCore.QMargins(dx, 0, dwidth, 0))
                 self.flash()
                 return True
 
@@ -76,22 +99,19 @@ class MainWindow(QtWidgets.QMainWindow):
                 self.flash()
                 return False
             else:
-                dx = (dwidth+(i%2))//2
-                button.setGeometry(button.geometry()+QtCore.QMargins(dx, 0, dwidth, 0))
+                dx = (dwidth+(i % 2))//2
+                button.setGeometry(button.geometry() +
+                                   QtCore.QMargins(dx, 0, dwidth, 0))
                 self.flash()
                 return True
-        
 
         for i in range(100):
             if not sub_width(i):
                 break
-        
-        button.setStyleSheet('QPushButton {'+' border-image: url("{}"); '.format('none')+'}')
+
+        button.setStyleSheet(
+            'QPushButton {'+' border-image: url("{}"); '.format('none')+'}')
 
         for i in range(100):
             if not add_width(i):
                 break
-
-
-    def play_sound(self):
-        sound_decode()
