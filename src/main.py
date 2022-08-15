889fad25c7c06692aedd03ab6d26528e666a3fb9
magical <sun920122@gmail.com>
2022-08-15 19:22:33 +0800

    Fix app icon show

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 
+# magical 2022.8
+
 import os
 import sys
+import ctypes
 
 from PyQt5 import QtWidgets
 
 from core import MainWindow
 
 if __name__ == '__main__':
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('arbitrary string')
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     os.system('cls' if os.name == 'nt' else 'clear')
